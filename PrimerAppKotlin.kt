710b55effa5a3214a964848f72915e91e109f7ed
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-07-22 12:05:07 -0600

    Commented code

--- PrimerAppKotlin.kt
+++ PrimerAppKotlin.kt
@@ -1,4 +1,13 @@
-// // No tocar esta clase ---
+/**
+ * *Copyright (C), 2022-2023, Fabian Juarez Tello (FabianJuarez182)
+    * @author   Fabian Juarez Tello
+    * FileName: DataListItemIdentification
+    @version: I
+    - Creation: 15/07/2022
+    - Last modification: 22/07/2022
+ */
+
+//  No tocar esta clase ---
 data class ItemData(
     var originalPos: Int,
     var originalValue: Any,
@@ -7,12 +16,12 @@ data class ItemData(
 )
 // -----------------------
 
-
+// Main function
 fun main() {
     val result = processList(listOf(20, 25, 2, 7, "hola", "", true, false, null, 2.0))
     println(result)
 }
-
+// Process list function for use in data splitting
 fun processList(inputList: List<Any?>?): List<ItemData>? {
     var i:Int = 0
     val finalList = ArrayList<ItemData>()
@@ -27,23 +36,25 @@ fun processList(inputList: List<Any?>?): List<ItemData>? {
             val element2 = when (element) {
                 is Int -> {
                    when{
-                        element%10==0 ->  ItemData(Pos, element, "entero", "M10")
-                        element%5 == 0 -> ItemData(Pos, element, "entero", "M5")
-                    	element%2 == 0 -> ItemData(Pos, element, "entero", "M2")
-                        else -> ItemData(Pos, element, "entero", null)
+                        element%10==0 ->  ItemData(Pos, element, "entero", "M10") //item creation
+                        element%5 == 0 -> ItemData(Pos, element, "entero", "M5") //item creation
+                    	element%2 == 0 -> ItemData(Pos, element, "entero", "M2") //item creation
+                        else -> ItemData(Pos, element, "entero", null) //item creation
                     }
                 }
                 is String ->{
-                    ItemData(Pos, element, "cadena","L" + element.length)} 
+                    ItemData(Pos, element, "cadena","L" + element.length)}  // item creation
                 
                 is Boolean ->{
-                    ItemData(Pos, element, "booleano",if (element) "Verdadero" else "Falso")}
+                    ItemData(Pos, element, "booleano",if (element) "Verdadero" else "Falso")} // item creation
                 
                 else -> { 
-                    ItemData(Pos, element,null, null)}
+                    ItemData(Pos, element,null, null)} // item creation
             }
+        // Addition of the element in the class
         finalList.add(element2)
         }
     }
+    // The final list will be returned with the data already evaluated.
     return finalList
 }
